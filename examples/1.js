49607d4670af519f8de50f011c0ad3d8bad6179f
3hhh <tripleh@hackingthe.net>
2021-01-11 20:34:13 +0100

    move details to separate html files

--- examples/1.js
+++ examples/1.js
@@ -5,144 +5,115 @@ graph TB
 dom0(dom0)
 class dom0 class_AdminVM;
 class dom0 label_black;
-click dom0 show_details "details"
 
 sys-dns(sys-dns)
 class sys-dns class_DispVM;
 class sys-dns label_yellow;
-click sys-dns show_details "details"
 
 sys-fw(sys-fw)
 class sys-fw class_DispVM;
 class sys-fw label_green;
-click sys-fw show_details "details"
 
 sys-fw-vpn(sys-fw-vpn)
 class sys-fw-vpn class_DispVM;
 class sys-fw-vpn label_green;
-click sys-fw-vpn show_details "details"
 
 sys-net(sys-net)
 class sys-net class_DispVM;
 class sys-net label_red;
-click sys-net show_details "details"
 
 sys-usb(sys-usb)
 class sys-usb class_DispVM;
 class sys-usb label_orange;
-click sys-usb show_details "details"
 
 sys-vpn(sys-vpn)
 class sys-vpn class_DispVM;
 class sys-vpn label_orange;
-click sys-vpn show_details "details"
 
 e-mail(e-mail)
 class e-mail class_AppVM;
 class e-mail label_yellow;
-click e-mail show_details "details"
 
 internal-dvm(internal-dvm)
 class internal-dvm class_AppVM;
 class internal-dvm label_green;
 class internal-dvm dispTemplate;
-click internal-dvm show_details "details"
 
 nonet-dvm(nonet-dvm)
 class nonet-dvm class_AppVM;
 class nonet-dvm label_blue;
 class nonet-dvm dispTemplate;
-click nonet-dvm show_details "details"
 
 Policy_Group_1>Policy Group 1]
 class Policy_Group_1 group_policies
-click Policy_Group_1 show_details "details"
 
 Policy_Group_2>Policy Group 2]
 class Policy_Group_2 group_policies
-click Policy_Group_2 show_details "details"
 
 Policy_Group_3>Policy Group 3]
 class Policy_Group_3 group_policies
-click Policy_Group_3 show_details "details"
 
 Policy_Group_4>Policy Group 4]
 class Policy_Group_4 group_policies
-click Policy_Group_4 show_details "details"
 
 Policy_Group_5>Policy Group 5]
 class Policy_Group_5 group_policies
-click Policy_Group_5 show_details "details"
 
 print-dvm(print-dvm)
 class print-dvm class_AppVM;
 class print-dvm label_orange;
 class print-dvm dispTemplate;
-click print-dvm show_details "details"
 
 surfing-dvm(surfing-dvm)
 class surfing-dvm class_AppVM;
 class surfing-dvm label_red;
 class surfing-dvm dispTemplate;
-click surfing-dvm show_details "details"
 
 sys-dns-dvm(sys-dns-dvm)
 class sys-dns-dvm class_AppVM;
 class sys-dns-dvm label_yellow;
 class sys-dns-dvm dispTemplate;
-click sys-dns-dvm show_details "details"
 
 sys-fw-dvm(sys-fw-dvm)
 class sys-fw-dvm class_AppVM;
 class sys-fw-dvm label_green;
 class sys-fw-dvm dispTemplate;
-click sys-fw-dvm show_details "details"
 
 sys-net-dvm(sys-net-dvm)
 class sys-net-dvm class_AppVM;
 class sys-net-dvm label_red;
 class sys-net-dvm dispTemplate;
-click sys-net-dvm show_details "details"
 
 sys-usb-dvm(sys-usb-dvm)
 class sys-usb-dvm class_AppVM;
 class sys-usb-dvm label_orange;
 class sys-usb-dvm dispTemplate;
-click sys-usb-dvm show_details "details"
 
 sys-vpn-dvm(sys-vpn-dvm)
 class sys-vpn-dvm class_AppVM;
 class sys-vpn-dvm label_orange;
 class sys-vpn-dvm dispTemplate;
-click sys-vpn-dvm show_details "details"
 
 VM_Group_1(VM Group 1)
 class VM_Group_1 group_vms
-click VM_Group_1 show_details "details"
 
 VM_Group_2(VM Group 2)
 class VM_Group_2 group_vms
-click VM_Group_2 show_details "details"
 
 VM_Group_3(VM Group 3)
 class VM_Group_3 group_vms
-click VM_Group_3 show_details "details"
 
 VM_Group_4(VM Group 4)
 class VM_Group_4 group_vms
-click VM_Group_4 show_details "details"
 
 VM_Group_5(VM Group 5)
 class VM_Group_5 group_vms
-click VM_Group_5 show_details "details"
 
 VM_Group_6(VM Group 6)
 class VM_Group_6 group_vms
-click VM_Group_6 show_details "details"
 
 VM_Group_7(VM Group 7)
 class VM_Group_7 group_vms
-click VM_Group_7 show_details "details"
 
 %% edges %%
 
